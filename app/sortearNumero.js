df7efe2a202be021027237a4570976273b547d72
Wellington2708 <wellingtondevelopercontact@gmail.com>
2022-11-14 19:32:22 +0000

    iniciando projeto do número secreto

---
+++ app/sortearNumero.js
@@ -0,0 +1,16 @@
+const menorValor = 1;
+const maiorValor = 100;
+
+const numeroSecreto = numeroAleatorio();
+
+function numeroAleatorio() {
+    return parseInt(Math.random() * maiorValor + 1);
+}
+
+console.log("O número é:", numeroSecreto);
+
+const elementoMenorValor = document.getElementById("menor-valor")
+elementoMenorValor.innerHTML = menorValor;
+
+const elementoMaiorValor = document.getElementById("maior-valor")
+elementoMaiorValor.innerHTML = maiorValor;
